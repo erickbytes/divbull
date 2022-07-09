af1bc549c026cbc2472e3542fb6b9943d96d5f5b
Erick Rumbold <erickrumbold@gmail.com>
2022-07-09 17:20:51 -0500

    updated pelican config file
    add site Favicon and subtitle to pelicanconf.py

--- divbull.com/pelicanconf.py
+++ divbull.com/pelicanconf.py
@@ -1,33 +1,43 @@
 AUTHOR = 'Erick'
 SITENAME = 'Diversified Bullish'
-SITEURL = ''
-
+SITESUBTITLE = 'An Investing and Stocks Blog'
+SITEURL = 'https://divbull.com'
 PATH = 'content'
-
 TIMEZONE = 'America/Chicago'
-
 DEFAULT_LANG = 'en'
+GITHUB_URL = 'http://github.com/erickbytes/'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+TAG_FEED_ATOM = 'feeds/{slug}.tag.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
+MENUITEMS = [
+    ['RSS', "feeds/all.rss.xml"],
+    ['Atom', "feeds/all.atom.xml"]
+]
 DEFAULT_PAGINATION = 10
-
 THEME = "/home/erick/Desktop/Projects/divbull/blue-penguin"
-
+STATIC_PATHS = [
+    'images',
+    'extra/robots.txt',
+    'extra/favicon.ico',
+]
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+}
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+# Blogroll
+# LINKS = (('Pelican', 'https://getpelican.com/'),
+#          ('Python.org', 'https://www.python.org/'),
+#          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+#          ('You can modify those links in your config file', '#'),)
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
