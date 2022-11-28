c432c3ab9faaf98f29638d5e0be848730b4d365b
Erick <erickrumbold@gmail.com>
2022-11-28 12:24:18 -0600

    Adding new post, 'Humans in the Balance Sheet'

---
+++ divbull.com/content/Humans-in-the-Balance-Sheet.md
@@ -0,0 +1,9 @@
+Title: Humans in the Balance Sheet 
+Date: 2022-11-28 12:21
+Category: Essay
+
+Corporations value humans, until times get tough. Then they start to cut. 
+
+I thought they were less brutal, deploying strategies so feudal. Employees are just a line item to them.
+
+What's good for the company? They'll land on their feet. They don't care about a human in the balance sheet.
--- divbull.com/new_post.py
+++ divbull.com/new_post.py
@@ -2,25 +2,25 @@ from datetime import date
 
 
 def post_name():
-	"""Ask for the new post's name."""
-	name = input("Enter the new post's title:\n")
-	return name
+    """Ask for the new post's name."""
+    name = input("Enter the new post's title:\n")
+    return name
 
 
 def post_template(title):
-	"""Return str, post draft"""
-	post = f"Title: {title} \nDate: {date.today()} 4:20 \nCategory: Essay"
-	return post
+    """Return str, post draft"""
+    post = f"Title: {title} \nDate: {date.today()} 4:20 \nCategory: Essay"
+    return post
 
 
 def save_draft(name, post):
-	"""Save new post draft to content folder."""
-	content = "/home/erickbytes/divbull/divbull.com/content"
-	name = name.replace(" ","-")
-	md = f"{content}/{name}.md"
-	with open(md,"w") as fhand:
-		fhand.write(post)
-	return None
+    """Save new post draft to content folder."""
+    content = "/home/erickbytes/divbull/divbull.com/content"
+    name = name.replace(" ", "-")
+    md = f"{content}/{name}.md"
+    with open(md, "w") as fhand:
+        fhand.write(post)
+    return None
 
 
 name = post_name()
